0128a1b755732bf6d11fca02f505a060d8cddc36
Vladimir <vladimirkukolovich@gmail.com>
2023-04-27 19:51:41 +0300

    feat: add createElement function

---
+++ src/js/createElement.ts
@@ -0,0 +1,16 @@
+type Key = {
+  element: string, className: string, innerHtml?: string, data?: string,
+};
+export const createElement = ({
+  element, className, innerHtml, data,
+}: Key) => {
+  const elem = document.createElement(element);
+  elem.className = className;
+  if (innerHtml) {
+    elem.innerHTML = innerHtml;
+  }
+  if (data) {
+    elem.dataset.keyCode = data;
+  }
+  return elem;
+};
